053cc358dab83c8f539e78deb9f22aaefc956c45
root <root@ZabsPC>
2023-01-30 17:11:19 -0800

    my first commit for lab-1

---
+++ lab1.cpp
@@ -0,0 +1,14 @@
+//Zabdiel Garcia
+//lab -1 jan 24
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int a = rand() % 10 + 1;
+    cout << a << endl;
+    cout << "Hello World ... the Update\n";
+
+    return 0;
+}
